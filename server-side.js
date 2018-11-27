cd1f4322d1a5727724301b776bac51fec8a4a3cd
Zafir Damani <zafird95@gmail.com>
2018-11-27 10:54:09 -0800

    made server side js

---
+++ server-side.js
@@ -0,0 +1,7 @@
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("txtHint").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "getcustomer.asp?q="+str, true);
+  xhttp.send();
